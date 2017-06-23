99f79d2474c7c1cae88519c52493f935461945c3
Gabe Blair <gabe@castironcoding.com>
2017-06-23 14:40:20 -0700

    [B] Remove unnecessary hook

--- ext_localconf.php
+++ ext_localconf.php
@@ -23,12 +23,6 @@ ExtensionUtility::configurePlugin(
 CustomContentElementUtility::addCustomContentElements($_EXTKEY, 'CIC');
 
 /**
- * Hook for assigning arbitrary stuff in frontend fluidpage templates
- */
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['fluidpage:Tx_Fluidpage_Controller_Template']['viewAssignmentHooks'][] =
-    'CIC\T3site\Hook\PageTemplateViewAssignment->getAssignments';
-
-/**
  * Hook to modify TCA after all extensions have been loaded. This does a few minor things like ensure certain news
  * config is in place.
  */
